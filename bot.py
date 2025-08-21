4594f266765007e6678ef851b8a1560f06dee5a2
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2025-08-21 16:16:50 +0200

    esit in bot.py

--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ TOKEN = os.getenv("TOKEN")
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.command()
