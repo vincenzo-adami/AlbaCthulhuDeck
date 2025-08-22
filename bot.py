1fa7ea2ab6bf531173c683864cdae74066a33578
vincenzo-adami <51445073+vincenzo-adami@users.noreply.github.com>
2025-08-22 19:58:43 +0200

    typo

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 from discord import app_commands
 from discord.ext import commands
 
-TOKEN = "IL_TUO_TOKEN"  # Puoi continuare a passarlo da Railway come variabile d'ambiente
+TOKEN = os.getenv("TOKEN")  # Puoi continuare a passarlo da Railway come variabile d'ambiente
 
 intents = discord.Intents.default()
 intents.message_content = True  # necessario se vuoi leggere i messaggi
